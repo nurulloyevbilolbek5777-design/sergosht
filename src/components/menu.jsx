0ce5b823a7d1cbadb3b735d8f42388235bf640b2
nurulloyevbilolbek5777-design <nurulloyevbilolbek5777@gmail.com>
2026-08-19 16:00:33 +0500

    menu.jsx

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,24 +1,61 @@
 import { NavLink } from "react-router";
 
-export default function Menu({ categories }) {
+export default function Menu({ categories = [] }) {
+  function getLinkClass({ isActive }) {
+    return isActive ? "active" : "";
+  }
+
   return (
     <aside className="menu menu-fixed">
+      <p className="menu-label">SAHIFALAR</p>
 
-      <p className="menu-label">Sahifalar</p>
       <ul className="menu-list">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/delivery">профиль</NavLink></li>
-        <li><NavLink to="/reviews">отзывы</NavLink></li>
-        <li><NavLink to="/promotions">акции</NavLink></li>
-        <li><NavLink to="/About">про Sergo'sht</NavLink></li>
-        <li><NavLink to="/Basket">корзина</NavLink></li>
+        <li>
+          <NavLink to="/" end className={getLinkClass}>
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/delivery" className={getLinkClass}>
+            Profil
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/reviews" className={getLinkClass}>
+            Fikrlar
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/promotions" className={getLinkClass}>
+            Aksiyalar
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/About" className={getLinkClass}>
+            Ser Go&apos;sht haqida
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/Basket" className={getLinkClass}>
+            Savat
+          </NavLink>
+        </li>
       </ul>
-       <p className="menu-label">Menu</p>
+
+      <p className="menu-label menu-label-space">MENU</p>
+
       <ul className="menu-list">
-        {categories.map(category => (
-          <li key={category.id}><a href={`#${category.slug}`}>{category.title}</a></li>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <a href={`#${category.slug}`}>{category.title}</a>
+          </li>
         ))}
       </ul>
     </aside>
-  )
+  );
 }
