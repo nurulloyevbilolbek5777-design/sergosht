12f24bb5271a9384feae8eba2dd0c6c43f7b3e56
nurulloyevbilolbek5777-design <nurulloyevbilolbek5777@gmail.com>
2026-08-19 15:56:43 +0500

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,33 @@
-import { createRoot } from 'react-dom/client'
-import Home from './components/Home'
-import './assets/bulma.min.css'
-import './style.css'
-import Delivery from './components/profile'
-import Reviews from './components/reviews'
-import Promotions from './components/promotions'
-import About from './components/About'
-import Basket from './components/Basket'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/delivery" element={<Delivery />} />
-            <Route path="/reviews" element={<Reviews />} />
-            <Route path="/promotions" element={<Promotions />} /> 
-            <Route path="/About" element={<About />} />
-            <Route path="/Basket" element={<Basket />} />
-        </Routes>
-    </BrowserRouter>
-)
+import Home from "./components/Home";
+import Delivery from "./components/profile";
+import Reviews from "./components/reviews";
+import Promotions from "./components/promotions";
+import About from "./components/About";
+import Basket from "./components/Basket";
+
+import "./assets/bulma.min.css";
+import "./style.css";
+
+const savedTheme = localStorage.getItem("darkTheme");
+
+if (savedTheme === "true") {
+  document.body.classList.add("dark-theme");
+} else {
+  document.body.classList.remove("dark-theme");
+}
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/delivery" element={<Delivery />} />
+      <Route path="/reviews" element={<Reviews />} />
+      <Route path="/promotions" element={<Promotions />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Basket" element={<Basket />} />
+    </Routes>
+  </BrowserRouter>
+);
